9a7c9c4767bfe29043e8ae5914857895c4d78e4e
Rayce Ramsay <ramsayrayce@gmail.com>
2026-07-14 20:48:12 -0600

    refactor: move web source code into src folder

--- apps/web/vitest.config.ts
+++ apps/web/vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: ['lib/**/*.test.ts'],
+    include: ['src/**/*.test.ts'],
     passWithNoTests: true,
   },
 });
